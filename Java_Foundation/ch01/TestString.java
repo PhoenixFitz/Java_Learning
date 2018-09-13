c6bebbe7b75714784add78835f016afa0d9f8a44
PhoenixFitz <wakl2004@163.com>
2018-09-13 23:02:38 +0800

    运算符

---
+++ Java_Foundation/ch01/TestString.java
@@ -0,0 +1,12 @@
+
+public class TestString{
+	public static void main(String[] args){
+	String a="3";
+	int b=4;
+	int c=5;
+	char d='a';
+	System.out.println(a+b+c); //345 bc全部转换为字符串进行连接
+	System.out.println(b+c+a); //93 
+	System.out.println(d+4); //97+4=101
+	}
+}
