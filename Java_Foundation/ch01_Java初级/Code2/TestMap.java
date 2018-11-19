75d5695f12131a6a40e90f1d5f1b0eeb42a154ef
PhoenixFitz <wakl2004@163.com>
2018-11-19 09:26:12 +0800

    Map接口2

---
+++ Java_Foundation/ch01_Java初级/Code2/TestMap.java
@@ -0,0 +1,37 @@
+﻿
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 测试HashMap的使用
+ *
+ */
+public class TestMap {
+
+    public static void main(String[] args){
+
+        Map<Integer,String> m1= new HashMap<>();
+        m1.put(1, "one");
+        m1.put(2, "two");
+        m1.put(3, "three");
+        m1.put(4, "four");
+
+        System.out.println(m1.get(1));
+
+        System.out.println(m1.size());
+        System.out.println(m1.isEmpty());
+        System.out.println(m1.containsKey(2));
+        System.out.println(m1.containsValue("five"));
+        System.out.println(m1);
+        Map<Integer,String> m2=new HashMap<>();
+        m2.put(6, "six");
+        m2.put(7, "seven");
+        m1.putAll(m2);
+        System.out.println(m1);
+        //map中的键是不能重复的，如果重复（是否重复是根据equals方法来判断的），则新的会覆盖旧的
+        m1.put(3, "三");
+        System.out.println(m1);
+
+    }
+}
---
+++ Java_Foundation/ch01_Java初级/Code2/TestMap2.java
@@ -0,0 +1,69 @@
+﻿
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 测试Map的常用方法
+ */
+public class TestMap2 {
+    public static void main(String[] args){
+
+        Employee e1=new Employee(1001, "Klaus", 50000);
+        Employee e2=new Employee(1002, "Fitz", 10000);
+        Employee e3=new Employee(1003, "Simmons", 8000);
+
+        Map<Integer,Employee> map =  new HashMap<>();
+
+        map.put(1001, e1);
+        map.put(1002, e2);
+        map.put(1003, e3);
+
+        Employee emp=map.get(1001);
+        System.out.println(emp.getEname());
+        System.out.println(map);
+
+    }
+
+}
+
+
+class Employee{
+    private int id;
+    private String ename;
+    private double salary;
+
+    public Employee(int id, String ename, double salary) {
+        this.id = id;
+        this.ename = ename;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "id:"+id+"name:"+ename+"薪水："+salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
