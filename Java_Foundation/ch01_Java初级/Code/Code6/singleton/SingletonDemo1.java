cf913b803928ea2f9bfb1322ef5b41052a976957
PhoenixFitz <wakl2004@163.com>
2019-02-21 22:22:22 +0800

    设计模式开始

---
+++ Java_Foundation/ch01_Java初级/Code/Code6/singleton/SingletonDemo1.java
@@ -0,0 +1,14 @@
+package com.klaus.singleton;
+
+public class SingletonDemo1 {
+    //类初始化时，立即加载这个对象（没有延时加载的优势）。加载类时，天然的是线程安全的！
+    private static SingletonDemo1 instance = new SingletonDemo1();
+
+    private SingletonDemo1(){
+
+    }
+
+    public static SingletonDemo1 getInstance(){
+        return instance;
+    }
+}
